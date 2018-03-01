5c7389cd464b5e8768adb6c8bc0455f41c17fcdd
ike76 <ike76@me.com>
2018-03-01 09:03:56 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path')
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json())
+
+// put routes here
+
+
+// catch everything else 
+app.get('*', (req, res)=>{
+	res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
+
+
+app.listen(process.env.PORT || 8080)
